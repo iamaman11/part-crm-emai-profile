ae6b53558e09e21628b4bdb54d9fdca106c7a9ef
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-08-11 17:05:51 +0300

    Add Mailbox contract exporter

---
+++ crates/control-plane-contract/src/bin/export_mailbox.rs
@@ -0,0 +1,7 @@
+#[path = "../mailbox_api.rs"]
+mod mailbox_api;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("{}", serde_json::to_string(&mailbox_api::openapi_fragment())?);
+    Ok(())
+}
