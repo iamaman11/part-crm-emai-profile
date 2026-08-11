f3b0b795a3340176d4eb1d85484402567da69f7b
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-08-11 17:34:37 +0300

    Use exported Mailbox contract in generator

--- crates/control-plane-contract/src/bin/export_mailbox.rs
+++ crates/control-plane-contract/src/bin/export_mailbox.rs
@@ -1,10 +1,6 @@
-#[path = "../mailbox_api.rs"]
-mod mailbox_api;
+use control_plane_contract::mailbox_api::openapi_fragment;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!(
-        "{}",
-        serde_json::to_string(&mailbox_api::openapi_fragment())?
-    );
+    println!("{}", serde_json::to_string(&openapi_fragment())?);
     Ok(())
 }
