567b585aab96f3fffe64c75699890106f77671f2
iamaman11 <44100369+iamaman11@users.noreply.github.com>
2026-08-28 14:27:20 +0300

    CAP-EXEC P2: satisfy canonical rustfmt import order

--- crates/application-ports/src/device_jobs.rs
+++ crates/application-ports/src/device_jobs.rs
@@ -1,6 +1,6 @@
 use core::{fmt, future::Future};
-pub use device_domain::{DeviceClaimId, DeviceJobId, DeviceJobTarget};
 use device_domain::DeviceJob;
+pub use device_domain::{DeviceClaimId, DeviceJobId, DeviceJobTarget};
 use profile_platform_primitives::{ActorContext, AggregateVersion, DeviceId, TenantId, UnixMillis};
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
